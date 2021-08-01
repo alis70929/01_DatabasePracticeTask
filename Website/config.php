f5ec46e5cf7474c3152684751827eb4e7d4ca983
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-08-02 10:37:10 +1200

    Working Topbit, Bottombit and config

--- Website/config.php
+++ Website/config.php
@@ -2,9 +2,9 @@
 
 ini_set('display_errors', 'On');
 
-define("DB_NAME","alis70929_Lvl2_Games")
-define("DB_USERNAME","alis70929")
-define("DB_PASSWORD","LhjeBlNpQZacZpDM")
-define("DB_HOST", "localhost")
+define("DB_NAME","alis70929_Lvl2_Games");
+define("DB_USERNAME","_alis70929");
+define("DB_PASSWORD","LhjeBlNpQZacZpDM");
+define("DB_HOST", "localhost");
 
 ?>
