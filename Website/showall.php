4621d7c92ea17334883e29db893a1d8b27adf832
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-08-04 15:09:34 +1200

    poggers,

--- Website/showall.php
+++ Website/showall.php
@@ -1,6 +1,10 @@
 <?php include("topbit.php");
 
-    $find_sql = "SELECT * FROM `game_details`";
+    $find_sql = "SELECT * FROM `game_details`
+    JOIN Genre ON (game_details.GenreID = Genre.GenreID)
+    JOIN Developer ON (game_details.DeveloperID = Developer.DeveloperID)
+    
+    ";
     $find_query = mysqli_query($dbconnect,$find_sql);
     $find_rs = mysqli_fetch_assoc($find_query);
     $count = mysqli_num_rows($find_query);
@@ -24,9 +28,86 @@
                 }
                 else{
                     do{
-                        
-                    }
-                }
+                        ?>
+                            <div class="results">
+
+                                <!-- Heading and subtitle -->
+                                <div class="flex-container">
+
+                                    <div>
+                                        <span class="sub_heading">
+                                            <a href= "<?php echo $find_rs['URL']; ?>">
+                                                <?php echo $find_rs['Name']; ?>
+                                            </a>
+                                        </span> 
+                                    </div> <!-- Close Title -->
+
+                                    <?php
+                                        if( $find_rs['Subtitle'] != "")
+                                        {
+                                            ?>
+                                            <div>
+                                            
+                                            &nbsp; &nbsp; | &nbsp; &nbsp;
+
+                                            <?php echo $find_rs['Name']; ?>
+
+                                            </div>
+                                            <?php
+                                        }
+                                    ?>
+
+                                </div> <!-- close Flex Container -->
+                            <p>
+                                <b>Price</b>
+                                
+                                <?php 
+                                    if($find_rs['Price'] == 0)
+                                    {
+                                        ?>
+                                        Free 
+                                            <?php
+                                                if( $find_rs['Purchases'] == 1)
+                                                {
+                                                    ?>
+                                                    (In App Purchases)
+                                                    <?php
+                                                }
+                                                
+                                            ?>
+                                        <?php
+                                    } 
+                                    else
+                                    {
+                                        ?>
+                                        $<?php echo $find_rs['Price']; ?>
+                                        <?php
+                                    } 
+                                
+                                ?>
+
+                                <br />
+                                <b>Genre</b>
+                                <?php echo $find_rs['GenreName']; ?>
+
+                                <br />
+
+                                <b>Developer</b>
+                                <?php echo $find_rs['DeveloperName']; ?>
+
+                                <br />
+
+                                <b>Rating</b>
+                                <?php echo $find_rs['Average User Rating']; ?> (based on <?php echo $find_rs['User Rating Count']; ?> votes)
+                            </p>
+                            <hr />
+                            <?php echo $find_rs['Description']; ?>
+                            </div>
+                            <br />
+                        <?php
+                    }// end results 'do'
+                    while($find_rs=mysqli_fetch_assoc($find_query));
+                }// end else
             ?>
             
 
