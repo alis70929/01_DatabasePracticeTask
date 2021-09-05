df6bc8212533ae82f3943a5f05136cc249b7e46b
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-09-06 10:34:46 +1200

    Pulled from prohect space cuz i forgot to commit

---
+++ Website/advanced.php
@@ -0,0 +1,98 @@
+<?php include("topbit.php");
+
+    $app_name = mysqli_real_escape_string($dbconnect,$_POST['app_name']);
+    $developer = mysqli_real_escape_string($dbconnect,$_POST['dev_name']);
+    $genre = $_POST['genre'];
+    $cost = mysqli_real_escape_string($dbconnect,$_POST['cost']);
+
+    if($cost=="")
+    {
+        $cost_op = ">=";
+        $cost = 0;
+    }
+    else
+    {
+        $cost_op = "<=";
+    }
+
+    if(isset($_POST['in_app']))
+    {
+        $in_app = 0;
+    }
+
+    else
+    {
+        $in_app = 1;
+    }
+
+    $rating_more_less = mysqli_real_escape_string($dbconnect, $_POST['rate_more_or_less']);
+    $rating = mysqli_real_escape_string($dbconnect,$_POST['rating']);
+
+    if($rating == "")
+    {
+        $rating = 0;
+        $rating_more_less = "at least";
+    }
+
+    if($rating_more_less == "at least"){
+        $rate_op = ">=";
+    }
+    elseif($rating_more_less == "at most")
+    {
+        $rate_op = "<=";
+    }
+    else{
+        $rate_op = ">=";
+        $rating  = 0;
+    }
+
+    $age_more_less = mysqli_real_escape_string($dbconnect, $_POST['age_more_or_less']);
+    $age = mysqli_real_escape_string($dbconnect,$_POST['age']);
+
+    if($age == "")
+    {
+        $age = 0;
+        $age_more_less = "at least";
+    }
+
+    if($age_more_less == "at least"){
+        $age_op = ">=";
+    }
+    else if($age_more_less == "at most")
+    {
+        $age_op = "<=";
+    }
+    else{
+        $age_op = ">=";
+        $age  = 0;
+    }
+
+    $find_sql = "SELECT * FROM `game_details`
+    JOIN Genre ON (game_details.GenreID = Genre.GenreID)
+    JOIN Developer ON (game_details.DeveloperID = Developer.DeveloperID)
+    WHERE `Name` LIKE '%$app_name%' 
+    AND `DeveloperName` LIKE '%$developer%'
+    AND `GenreName` LIKE '$genre'
+    AND `Price` $cost_op '$cost'
+    AND (`Purchases` = $in_app OR `Purchases` = 0)
+    AND `Average User Rating` $rate_op $rating
+    AND `Age Rating` $age_op $age
+    ";
+
+    $find_query = mysqli_query($dbconnect,$find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+?>
+                       
+            
+        <div class="box main">
+            <h2>Advanced Results</h2>
+            
+            
+            <?php include("results.php") ?>
+            
+
+            
+        </div> <!-- / main -->
+        
+<?php include("bottombit.php") ?>
--- Website/bottombit.php
+++ Website/bottombit.php
@@ -16,6 +16,101 @@
 
            </form>
 
+           <br />
+           <hr />
+           <br />
+
+            <div class="advanced-frame">
+            
+            <h2>Advanced Search</h2>
+            <form class="searchform" method="post" action="advanced.php" enctype="multipart/form-data">
+                
+                
+                <input class="adv" type="text" name="app_name" size="40"value=""  placeholder="App Name/Title..." />
+
+                <input class="adv" type="text" name="dev_name" size="40"value=""  placeholder="Developer..." />
+                
+                <!-- Genre Seacrh -->
+                <select class="search adv" name="genre">
+
+                <option value="" selected>Genre...</option>
+                    <?php 
+                     $genre_sql="SELECT * FROM `Genre` ORDER BY `Genre`.`GenreName` ASC";
+                     $genre_query=mysqli_query($dbconnect,$genre_sql);
+                     $genre_rs=mysqli_fetch_assoc($genre_query);
+                    
+                     do {
+                        ?>
+                            <option value="<?php echo $genre_rs['GenreName']; ?>">
+                            <?php echo $genre_rs['GenreName']; ?></option>
+                        <?php
+                     }
+
+                     while($genre_rs=mysqli_fetch_assoc($genre_query))
+                    ?>
+                </select>
+
+                <!-- Cost -->
+                <div class="flex-container">
+                
+                     <div class="adv-text">
+                        Cost&nbsp;(less&nbsp;than):
+                     </div>
+
+                     <div>
+                        <input class="adv" type="text" name="cost" size="40"value=""  placeholder="$..." />
+                     </div>
+                
+                </div>
+
+                <!-- In App -->
+                <input class="adv-text" type="checkbox" name="in_app" value="0"/> No In App Purchases
+                
+                <div class="flex-container">
+                     <div class="adv-text">
+                     Rating:
+                     </div>
+                        
+                     <div>
+                        <select class="search adv" name="rate_more_or_less">
+                            <option value="" disabled selected>Choose...</option>
+                            <option value="at least" >At Least</option>
+                            <option value="at most">At Most</option>
+                        </select>
+                    </div>
+
+                     <div>
+                        <input class="adv" type="text" name="rating" size="2"value=""  placeholder="" />
+                     </div>
+                </div>
+
+                <div class="flex-container">
+                     <div class="adv-text">
+                     Age:
+                     </div>
+                        
+                     <div>
+                        <select class="search adv" name="age_more_or_less">
+                            <option value="" selected>Choose...</option>
+                            <option value="at least" >At Least</option>
+                            <option value="at most" >At Most</option>
+                        </select>
+                    </div>
+
+                     <div>
+                        <input class="adv" type="text" name="age" size="2"value=""  placeholder="" />
+                     </div>
+                </div>
+
+                <input class="submit advanced-button" type="submit" name="advanced" value="Search &nbsp; &#xf002;" />
+
+
+
+            </form> <!-- End of Advanced Search -->
+            
+            
+            
+            </div> <!-- Advanced Frame End --->
             
         </div> <!-- / side bar -->
         
